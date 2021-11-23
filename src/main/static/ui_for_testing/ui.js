ae4fe3b5653d89ef7a234d9e399b744dc8f572ad
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-11-23 17:04:25 +0200

    Add event listeners

--- src/main/static/ui_for_testing/ui.js
+++ src/main/static/ui_for_testing/ui.js
@@ -1,23 +1,39 @@
 (function () {
     O = Object, doc = document
+    const atlasMap = AtlasMap()
 
     window.onload = function () {
         renderBirdList()
     }
 
     async function renderBirdList() {
-        const leafletMap = await initLeafletMap()
         const response = await fetch("/api/birds")
         const json = await response.json()
         const birdTable = doc.getElementById('bird-table')
-        const contents = createTableContents(json, getColumnModel(birdTable, "data-col"))
-        birdTable.getElementsByTagName("tbody")[0].appendChild(contents)
+        const rows = createTableRows(json, getColumnModel(birdTable, "data-col"))
+        const atlasMapSvgImage = await atlasMap.getSvgImage()
+        const leafletMap = initLeafletMap(atlasMapSvgImage)
+        const changeMapData = () => atlasMapSvgImage.getSvgElement().querySelectorAll("circle").forEach(e => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16)
+            e.setAttribute('style', 'fill:#' + randomColor)
+            e.setAttribute('display', 'block')
+        })
+        rows.forEach(row => row.addEventListener('click', changeMapData))
+        const fragment = rows.reduce((fragment, row) =>
+            fragment.appendChild(row).parentNode, doc.createDocumentFragment())
+        birdTable.getElementsByTagName("tbody")[0].appendChild(fragment)
     }
 
-    async function initLeafletMap() {
-        const mapResponse = await fetch("/api/grid/map")
-        atlasMapSvgImage = SvgImage(await mapResponse.text())
-        mapService = MapService(atlasMapSvgImage)
+    function AtlasMap() {
+        let atlasMap
+        const getSvgImage = async () =>
+            typeof atlasMap === 'undefined' ? await fetchAtlasMapSvgImage() : atlasMap
+        const fetchAtlasMapSvgImage = async () =>
+            atlasMap = SvgImage(await (await fetch("/api/grid/map")).text())
+        return {getSvgImage}
+    }
+
+    function initLeafletMap(atlasMapSvgImage) {
         const leafletMap = L.map('atlas-map', {crs: L.CRS.Simple})
         const bounds = [[0, 0], [atlasMapSvgImage.getHeight(), atlasMapSvgImage.getWidth()]]
         L.svgOverlay(atlasMapSvgImage.getSvgElement(), bounds).addTo(leafletMap)
@@ -33,7 +49,7 @@
         return toModel(headerCells)
     }
 
-    function createTableContents(data, columnModel) {
+    function createTableRows(data, columnModel, rowListener) {
         const toCell = text => O.assign(doc.createElement('td'), {textContent: text}),
               toRow = (row, cell) => row.appendChild(cell).parentElement,
               reorderProps = model => object => O.assign({}, model, object),
@@ -41,7 +57,6 @@
               toForm = model => pipe(reorderProps(model), O.entries, removeExtraProps(model), O.fromEntries)
         return data.map(toForm(columnModel))
                 .map(object => O.values(object).map(toCell).reduce(toRow, doc.createElement('tr')))
-                .reduce((fragment, row) => fragment.appendChild(row).parentNode, doc.createDocumentFragment())
     }
 
     function pipe(...functions) {
